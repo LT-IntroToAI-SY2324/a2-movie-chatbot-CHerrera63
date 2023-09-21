6f93c826abfa4b340a7c146ca030ae838185204d
CHerrera63 <cherrera63@cps.edu>
2023-09-21 10:17:31 -0500

    Finished/tried a2.py

--- a2.py
+++ a2.py
@@ -21,32 +21,70 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
     # keep checking as long as we haven't hit the end of either pattern or source while
     # pind is still a valid index OR sind is still a valid index (valid index means that
     # the index is != to the length of the list)
-    while "FILL IN CONDITION HERE":
+    while pind <= len(pattern)-1 or sind <= len(source)-1:
+
         # your job is to fill out the body of this loop
 
         # you should delete the following line
-        return ["Not done yet :)"]
+        # return ["Not done yet :)"]
 
         # 1) if we reached the end of the pattern but not source
+            # if pind <= len(pattern) and sind < len(source):
+            if pind == len(pattern) and sind < len(source):
+                return None
 
         # 2) if the current thing in the pattern is a %
         # WARNING: this condition contains the bulk of the code for the assignment
         # If you get stuck on this one, we encourage you to attempt the other conditions
         #   and come back to this one afterwards
+            elif pattern[pind] == "%":
+                 if pind == len(pattern) - 1:
+                      result += [" ".join(source[sind:])]
+                      return result
+                 else:
+                      accum = ""
+                      pind = pind +1
+                      while pattern[pind] != source[sind]:
+                           accum += source[sind] + " "
+                           sind = sind +1
+                           if sind >= len(source):
+                                return None
+                      result.append(accum.strip())
+                #  while sind <= len(source)-1:
+                #       result.append(source[sind])
+                #       if len(result) > 1:
+                #         p1 = result[0]
+                #         p2 = result[1]
+                #         add = p1+ " " +p2
+                #         result = [add]
+                #       if pind + 1 != len(pattern):
+                #            pind = pind + 1
+                #       sind = sind +1
+                #  pind = pind + 1
 
         # 3) if we reached the end of the source but not the pattern
+            elif sind == len(source) and pind < len(pattern):
+                return None
 
         # 4) if the current thing in the pattern is an _
+            elif pattern[pind] == "_":
+                 result.append(source[sind])
+                 sind = sind +1
+                 pind = pind +1
 
         # 5) if the current thing in the pattern is the same as the current thing in the
         # source
+            elif pattern[pind] == source[sind]:
+                sind = sind +1
+                pind = pind +1
 
         # 6) else : this will happen if none of the other conditions are met it
         # indicates the current thing it pattern doesn't match the current thing in
         # source
+            else:
+                 return None
 
     return result
-
 
 if __name__ == "__main__":
     assert match(["x", "y", "z"], ["x", "y", "z"]) == [], "test 1 failed"
@@ -71,5 +109,6 @@ if __name__ == "__main__":
     # this last case is a strange one, but it exposes an issue with the way we've
     # written our match function
     assert match(["x", "%", "z"], ["x", "y", "z", "z", "z"]) == None, "test 15 failed"
+    # assert match(["%"])
 
     print("All tests passed!")
